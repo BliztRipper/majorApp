e63df4e20f696753206f8670fd723f66a58a2720
Arnon Tawong <arnontawong@agc01233.ebusiness.th>
2018-07-04 14:41:32 +0700

    complete integrated movie list

--- dist/data_carousel.js
+++ dist/data_carousel.js
@@ -3,20 +3,32 @@ const movieData = 'http://cinema.truemoney.net/MovieList';
 (async ()=>{
   let getComingSoon_data = await fetch(movieData)
   .then(res => res.json())
-  .then(comingsoon => comingsoon.data.comingsoon)  
-  for (let i = 0; i < 5; i++) {
-    let movieCarousel = document.getElementsByClassName("slick-track");
-    let comingsoonCarousel = `
-    <figure> 
-      <div class="container">
-        <img class="d-block mx-auto poster" src="${getComingSoon_data[i].poster_ori}" alt="${getComingSoon_data[i].title_en}"/>
-        <div class="d-flex justify-content-center align-items-center flex-column movie-detail"><a class="btn booking-btn" href="showing-time.html">ซื้อตั๋ว</a>
-          <h2 class="pt-2 movie-detail__title">${getComingSoon_data[i].title_en}</h2>
-          <h5 class="movie-detail__sec-title">${getComingSoon_data[i].title_th}</h5>
-          <span class="movie-detail__genre">${getComingSoon_data[i].genre}</span>
+  .then(comingsoon => comingsoon.data.now_showing)
+    for (let i = 0; i < getComingSoon_data.length;  i++) {
+      let movieCarousel = document.getElementById('movie-carousel')
+      if (getComingSoon_data[i].poster_ori == ''){
+        getComingSoon_data[i].poster_ori = 'images/Placeholder.jpg'
+      }
+      let comingsoonCarousel = `
+      <figure> 
+        <div class="container">
+          <img class="d-block mx-auto poster" src="${getComingSoon_data[i].poster_ori}" alt="${getComingSoon_data[i].title_en}"/>
+          <div class="d-flex justify-content-center align-items-center flex-column movie-detail"><a class="btn booking-btn" href="showing-time.html">ซื้อตั๋ว</a>
+            <h2 class="pt-2 movie-detail__title text-center">${getComingSoon_data[i].title_en}</h2>
+            <h5 class="movie-detail__sec-title">${getComingSoon_data[i].title_th}</h5>
+            <span class="movie-detail__genre">${getComingSoon_data[i].genre}</span>
+          </div>
         </div>
-      </div>
-    </figure>`
-    movieCarousel.innerHTML += comingsoonCarousel
-  }
+      </figure>`
+      movieCarousel.innerHTML += comingsoonCarousel 
+    } 
+    $('#movie-carousel').slick({
+      dots: false,
+      arrows: false,
+      infinite: false,
+      speed: 300,
+      centerMode: true,
+      lazyLoad: 'ondemand', 
+      slidesToScroll: 1,
+    }); 
 })();
--- src/main.js
+++ src/main.js
@@ -2,19 +2,6 @@
 //@prepros-prepend ../node_modules/bootstrap/dist/js/bootstrap.bundle.js
 //@prepros-prepend ../node_modules/slick-carousel/slick/slick.min.js
 
-/*jQuery throttle / debounce - v1.1 - 3/7/2010*/
-(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
-
-$('#movie-carousel').slick({
-  dots: false,
-  arrows: false,
-  infinite: false,
-  speed: 300,
-  centerMode: true,
-  lazyLoad: 'ondemand', 
-  slidesToScroll: 1,
-}); 
-
 $(document).ready(function(){
   $('.bottom-navtab').click(function(e){
     $('.bottom-navtab').css("border-top","")
